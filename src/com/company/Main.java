64229d34e794dbcaf4e64a57269acd57eeafc6de
Abhishek <ay129695004252@gmail.com>
2021-08-14 20:40:01 +0530

    First project

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+class main{
+    public static void main(String[] args){
+         System.out.println("The sum of these number is");
+
+         int num1 = 5;
+         int num2 = 4;
+         int num3 = 5;
+         int sum = num1 + num2 + num3;
+         System.out.println(sum);
+    }
+}
